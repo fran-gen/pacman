504e3bd78918e555b275ca29c5622637d290c63c
fran-gen <fra.fran.francesco@gmail.com>
2023-11-13 23:51:08 +0100

    set game's mode and time.

--- pacman.py
+++ pacman.py
@@ -4,3 +4,8 @@ pygame.init()
 
 WIDTH = 900
 HEIGHT = 950
+
+# Set the game to have those static dimensions
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+# Set the game's speed
+timer = pygame.time.Clock()
