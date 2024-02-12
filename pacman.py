943e663d8a73e1333ea95481f384e85c038eb6e0
fran-gen <fra.fran.francesco@gmail.com>
2024-02-13 00:25:39 +0100

    setting up a basic game window, maintaining a consistent frame rate, handling events such as quitting the game, and updating the display accordingly.

--- pacman.py
+++ pacman.py
@@ -4,8 +4,30 @@ pygame.init()
 
 WIDTH = 900
 HEIGHT = 950
-
-# Set the game to have those static dimensions
+# set the game to have those static dimensions
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
-# Set the game's speed
+# set the game's speed
 timer = pygame.time.Clock()
+# max speed
+fps = 60
+font = pygame.Font('freesansbold.ttf', 20)
+
+run = True
+while run:
+    # control the frame rate
+    timer.tick(fps)
+    # fill the background color
+    screen.fill('black')
+
+    # avoid infinite while loop
+    # event.get() gets anything pygame can process
+    for event in pygame.event.get():
+        # pygame.QUIT is a red cross to leave the game
+        if event.type == pygame.QUIT:
+            run = False
+
+    # draw on the screen every iteration
+    pygame.display.flip()
+# deinitialize all Pygame modules
+pygame.quit()
+
